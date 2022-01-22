39c362f0e69762ba3705ff3642cfe2240ecfbab8
janvi <janvi.kotecha0@gmail.com>
2022-01-22 14:04:23 +0530

    React counter  app

--- src/counter.js
+++ src/counter.js
@@ -1,31 +1,55 @@
 import React from 'react';
 
 class Counter extends React.Component {
-  state = { count: 0 }
+  state = { count: 0 };
 
   increment = () => {
     this.setState({
-      count: this.state.count + 1
+      count: this.state.count + 1,
     });
-  }
+  };
 
   decrement = () => {
     this.setState({
-      count: this.state.count - 1
+      count: this.state.count - 1,
     });
-  }
+  };
 
   render() {
-    return (
-      <div>
-        <h2>Counter</h2>
-        <div>
-          <button onClick={this.decrement}>-</button>
-          <span>{this.state.count}</span>
-          <button onClick={this.increment}>+</button>
-        </div>
-      </div>
-    )
+    return (    
+        <center>
+          <div
+            style={{
+              backgroundColor: '#cccc',
+              color: 'black',
+              padding: '50px',
+            }}
+          >
+            <h2>Counter</h2>
+            <h2>
+              <button
+                onClick={this.decrement}
+                style={{
+                  backgroundColor: 'lightblue',
+                  color: 'black',
+                  margin: '20px',
+                  width:'50px',
+                  padding:'6px'
+                }} >
+                -
+              </button>
+              {this.state.count}
+              <button onClick={this.increment}  style={{
+                  backgroundColor: 'lightblue',
+                  color: 'black',
+                  margin: '20px',
+                  width:'50px',
+                  padding:'6px'
+                }}>+</button>
+            </h2>
+          </div>
+        </center>
+    );
   }
 }
 
