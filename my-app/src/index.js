67fc8daa2bc5b42412e2c09c0d7fb536da037868
janvi <janvi.kotecha0@gmail.com>
2022-01-22 17:07:32 +0530

    demo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Counter from './Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+// store.dispatch({type : "INCREMENT"});
+// store.dispatch({type : "DECREMENT"});
 
 const App = () => (
-  <div>
-    <Counter />
-  </div>
+  <Provider store={store}>
+    <Counter/>
+  </Provider>
 );
+
 
 render(<App />, document.getElementById('root'));
